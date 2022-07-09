5bd23e6c78eedfcee7606d31adfcec597666d589
oleksandr_shchur <osur12709@gmail.com>
2022-07-09 16:42:14 +0300

    add bot configuration options

---
+++ RegymBot/Configurations/BotConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace RegymBot.Configurations
+{
+    public class BotConfiguration
+    {
+        public string Token { get; init; }
+        public string HostAddress { get; init; }
+    }
+}
