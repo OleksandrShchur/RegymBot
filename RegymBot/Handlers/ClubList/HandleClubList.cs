dd6c70d3dafdb2af8ed1fc06405e3fb31f785f15
oleksandr_shchur <osur12709@gmail.com>
2022-07-26 00:16:46 +0300

    fix club contacts return back button

--- RegymBot/Handlers/ClubList/HandleClubList.cs
+++ RegymBot/Handlers/ClubList/HandleClubList.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
+using RegymBot.Data.Enums;
+using RegymBot.Data.Repositories;
+using RegymBot.Helpers.Buttons;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -8,17 +11,27 @@ namespace RegymBot.Handlers.ClubList
 {
     public class HandleClubList : BaseHandle<HandleClubList>
     {
+        private readonly StaticMessageRepository _staticMessageRepository;
 
         public HandleClubList(ILogger<HandleClubList> logger,
-            ITelegramBotClient botClient)
+            ITelegramBotClient botClient,
+            StaticMessageRepository staticMessageRepository)
                 : base(logger, botClient)
-        { }
+        {
+            _staticMessageRepository = staticMessageRepository;
+        }
 
         public async Task BotOnClubList(Message message)
         {
             _logger.LogInformation("Receive message type in club list: {MessageType}", message.Type);
             if (message.Type != MessageType.Text)
                 return;
+
+            string text = await _staticMessageRepository.GetMessageByTypeAsync(BotPage.SelectClubPage);
+
+            await _botClient.SendTextMessageAsync(chatId: message.Chat.Id,
+                                            text: text,
+                                            replyMarkup: ClubButtons.Keyboard);
         }
     }
 }
