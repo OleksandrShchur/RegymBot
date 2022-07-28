3d44e8d3f1ffe5c602924520a12968b8f322869d
oleksandr_shchur <osur12709@gmail.com>
2022-07-28 23:23:28 +0300

    Add default seeded data to db

---
+++ RegymBot/Data/DbInitialize/DbInitializer.cs
@@ -0,0 +1,135 @@
+﻿using RegymBot.Data.Entities;
+using RegymBot.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegymBot.Data.DbInitialize
+{
+    public static class DbInitializer
+    {
+        public static void Seed(AppDbContext dbContext)
+        {
+            dbContext.Database.EnsureCreated();
+
+            if (dbContext.StaticMessages.Any())
+            {
+                return; // DB has been seeded
+            }
+
+            var staticMessages = new StaticMessageEntity[]
+            {
+                new StaticMessageEntity
+                {
+                    Page = BotPage.StartPage,
+                    Message = "Виберіть:"
+                },
+                new StaticMessageEntity
+                {
+                    Page = BotPage.MassagePage,
+                    Message = "В наших залах в PSHKN центр и ТРЦ Вавилон вы можете посетить массажные кабинеты, в которых вам помогут расслабиться после тренировки с помощью пятнадцати видов массажа."
+                },
+                new StaticMessageEntity
+                {
+                    Page = BotPage.PricePage,
+                    Message = "Прайс лист тренувань:"
+                },
+                new StaticMessageEntity
+                {
+                    Page = BotPage.LeaveFeedbackPage,
+                    Message = "Расскажите, что вам понравилось и как нам стать лутше для вас?"
+                },
+                new StaticMessageEntity
+                {
+                    Page = BotPage.SelectClubPage,
+                    Message = "Выберите клуб:"
+                },
+                new StaticMessageEntity
+                {
+                    Page = BotPage.SolariumPage,
+                    Message = "В наших залах в Аполло и ТРЦ Вавилон вы можете посетить солярий."
+                },
+                new StaticMessageEntity
+                {
+                    Page = BotPage.Club_Apollo,
+                    Message = "Телефон Аполло\n+380999999999\n\"Адрес:\nул. Хмельницкого 68\nРасписание:\nПН - СБ: 08 - 21\nВС: 9 - 18\""
+                },
+                new StaticMessageEntity
+                {
+                    Page = BotPage.Club_Vavylon,
+                    Message = "Телефон Вавилон\n+380999999999\n\"Адрес:\nул. Хмельницкого 68\nРасписание:\nПН - СБ: 08 - 21\nВС: 9 - 18\""
+                },
+                new StaticMessageEntity
+                {
+                    Page = BotPage.Club_Pshkn,
+                    Message = "Телефон PSHKN\n+380999999999\n\"Адрес:\nул. Хмельницкого 68\nРасписание:\nПН - СБ: 08 - 21\nВС: 9 - 18\""
+                }
+            };
+
+            dbContext.StaticMessages.AddRange(staticMessages);
+
+            var prices = new PriceEntity[]
+            {
+                new PriceEntity
+                {
+                    PriceName = "Позиція 2 (опис 2)",
+                    Price = 120,
+                    PriceType = PriceItem.Training
+                },
+                new PriceEntity
+                {
+                    PriceName = "Позиція солярій 1 (опис 1)",
+                    Price = 278,
+                    PriceType = PriceItem.Solarium
+                },
+                new PriceEntity
+                {
+                    PriceName = "Позиція 4 (опис 5)",
+                    Price = 178,
+                    PriceType = PriceItem.Training
+                },
+                new PriceEntity
+                {
+                    PriceName = "Позиція масаж 4 (опис 4)",
+                    Price = 240,
+                    PriceType = PriceItem.Massage
+                },
+                new PriceEntity
+                {
+                    PriceName = "Позиція 3 (опис 3)",
+                    Price = 490,
+                    PriceType = PriceItem.Training
+                },
+                new PriceEntity
+                {
+                    PriceName = "Позиція солярій 1 (опис 1)",
+                    Price = 400,
+                    PriceType = PriceItem.Solarium
+                },
+                new PriceEntity
+                {
+                    PriceName = "Позиція солярій 3 (опис 4)",
+                    Price = 680,
+                    PriceType = PriceItem.Solarium
+                },
+                new PriceEntity
+                {
+                    PriceName = "Позиція 1 (опис 1)",
+                    Price = 395,
+                    PriceType = PriceItem.Training
+                },
+                new PriceEntity
+                {
+                    PriceName = "Позиція масаж 1 (опис 1)",
+                    Price = 599,
+                    PriceType = PriceItem.Massage
+                },
+            };
+
+            dbContext.Prices.AddRange(prices);
+
+            dbContext.SaveChanges();
+        }
+    }
+}
--- RegymBot/Program.cs
+++ RegymBot/Program.cs
@@ -3,6 +3,11 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NLog.Web;
 using Microsoft.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+using RegymBot.Data;
+using RegymBot.Data.DbInitialize;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace RegymBot
 {
@@ -10,7 +15,25 @@ namespace RegymBot
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var dbContext = services.GetRequiredService<AppDbContext>();
+                    dbContext.Database.Migrate();
+                    DbInitializer.Seed(dbContext);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateHostBuilder(string[] args) =>
